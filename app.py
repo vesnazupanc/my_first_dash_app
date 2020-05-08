bb1015001083d5536e7cd033047fa6f199d8174f
vesnazupanc <vesnazupanc93@gmail.com>
2020-05-08 15:51:58 +0200

    dodani komentarji grafov

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ besedilo_uvod4 = '''
 
 ###### **Grafični prikaz 1**
 
-Točkovni grafični prikaz ponuja primerjavo med grobo incidenčno stopnjo in grobo umrljivostno stopnjo glede na
+Točkovni grafični prikaz ponuja primerjavo med incidenco in umrljivostjo glede na
 lokacijo raka od leta 1985 do leta 2016. 
 Prav tako ponuja tudi izbiro spola, kjer s tem lahko filtriramo podatke na ženski ali moški spol, podatke pa lahko
 pogledamo tudi za oba spola skupaj. 
@@ -95,35 +95,65 @@ Prav tako lahko izbiramo med leti od 1985 do leta 2016 ali pa s klikom na gumb "
 
 besedilo_pojasnila1 = '''
 ##### **INCIDENCA**
-**Incidenca** je mera, ki izraža absolutno število vseh novih primerov neke bolezni v časovnem intervalu, ponavadi koledarskem letu.
-Ker incidenca ne meri število bolnikov, se lahko zgodi, da isti bolnik v incidenco prispeva več primerov, če zboli za različnimi raki.  
+**Incidenca** je mera, ki izraža absolutno število vseh novih primerov neke bolezni v časovnem intervalu, 
+ponavadi koledarskem letu. Ker incidenca ne meri število bolnikov, se lahko zgodi, da isti bolnik v 
+incidenco prispeva več primerov, če zboli za različnimi raki. **Groba incidenčna stopnja** je mera, 
+ki absolutno velikost incidence v populaciji preračuna glede na 100 tisoč prebivalcev. 
+Groba incidenčna stopnja nam omogoča lažjo primerljivost med geografskimi in časovnimi območji.  
 '''
 besedilo_pojasnila2 = '''
 ##### **UMRLJIVOST**
-Umrljivost je mera, ki izraža absolutno število umrlih v neki populaciji, kot posledica neke bolezni, 
+**Umrljivost** je mera, ki izraža absolutno število umrlih v neki populaciji, kot posledica neke bolezni, 
 v našem primeru raka, v določenem časovnem obdobju, najbolj pogosto se za časovno obdobje uporablja koledarsko leto. 
+Analogno grobi incidenčni stopnji, tudi v tem primeru definiramo **Grobo umrljivostno stopnjo**, 
+ki absolutno število, po enaki enačbi, preračuna na 100 tisoč prebivalcev. 
+Med merami umrljivosti poznamo še starostno standardizirano umrljivostno stopnjo. 
 '''
 besedilo_pojasnila3 = '''
 ##### **PREVALENCA**
-Prevalenca je mera, ki nam pove, koliko bolnikov, s postavljeno diagnozo, je bilo na določen datum še živih. 
+**Prevalenca** je mera, ki nam pove, koliko bolnikov, s postavljeno diagnozo, je bilo na določen datum še živih. 
 Ponavadi je ta datum zadnji dan v letu. Za mere prevalence ni pomembno, kdaj je oseba zbolela.
 '''
 
-besedilo_p1_zenske = '''
-Pri ženskem spolu vidimo napredek pri trmtrm
+komentar1 = '''
+Breme raka se je med letoma 1985 in 2016 v Sloveniji povečalo. 
+Pri obeh spolih je največja umrljivost za vrsto raka, ki spada v skupino **sapnik, sapnici in pljuča**.
+Tu je vredno omembe, da v skupino ne spada rak mezoteliom, za katerega je znano, da je glavni povzročitelj azbest. 
+Mezoteliom ne spada v osnovno razdelitev raka, zato ni posebej prikazan. 
 
-test1
+Pri moških ima opazno povečanje incidence rak **prostate**, kjer pa incidenca narašča hitreje kot umrljivost.
+
+Opazno je tudi veliko povečanje incidence pri **drugih malignih neoplazmah kože**, je pa tu umrljivost skozi leta
+konstantno nizka.
+
+**Raki iz presejalnih programov**:
+
+Pri **materničnem vratu** vidimo, da padata tako incidenca kot umrljivost, kar kaže uspeh presejalnega programa **ZORA**.
+
+Pri raku **dojke** je videti upočasnitev rasti umrljivosti, kar kaže na učinkovitost presejalnega programa **DORA**
 '''
 
-besedilo_p1_moski = '''
-deci
+komentar2 = '''
+Glede na grobo incidenčno stopnjo in grobo umrljivosto stopnjo opazimo, da je v splošnem rak pogostejši
+pri moških, kot pri ženskah. Posledično je tudi umrljivost pri moških večja. Opazimo lahko tudi, da se je 
+pri moških zelo povečalo breme raka **prostate**. 
+
+Prevalenca se v splošnem z leti povečuje, kar pa je posledica tudi večje incidence. Zato si moramo pogledati obe meri,
+v kolikor nekaj sklepati. 
+
+**Raki iz presejalnih programov**:
+
+Pri raku **materničnega vratu** lako opazimo da se incidenca in groba incidenčna stopnja z leti zmanjšujeta, prevalenca pa se 
+povečuje. To nakazuje, da je presejalni program **ZORA** zelo uspešen.
+
+Pri raku **dojke** se incidenca in groba incidenčna stopnja sicer z leti povečujeta, vendar pa se prevalenca povečuje hitreje,
+groba stopnja umrljivosti pa je v zadnjih letih celo padla. Tudi to nakazuje na uspešnost presejalnega programa **DORA**.
+
+Uspešnost programa **SVIT** je trenutno težje oceniti, vendar bo le to najverjetneje močneje opazno čez nekaj let, ko
+bomo imeli na voljo več podatkov.
 '''
 
-besedilo_p1_skupaj = '''
-skupaj
-'''
 
-besedilo_dict_p1 = {'male': besedilo_p1_moski, 'female': besedilo_p1_zenske, 'both': besedilo_p1_skupaj}
 
 # =============================================================================
 # APLIKACIJA
@@ -169,8 +199,8 @@ tab3 = html.Div([
                                options=[{'label': spol_dict[i], 'value': i} for i in ['male', 'female', 'both']],
                                value='both',
                                clearable=False),
-                           html.H5('Komentaji:'),
-                           html.Div(id='komentarji-p1')
+                           html.H5('Komentar:'),
+                           html.Div([dcc.Markdown(komentar1)], style={'padding': '0px 0px 20px 0px'})
                            ]),
         html.Div(className="nine columns", style={'height': '1000px'},
                  children=[dcc.Graph(id='plot1', style={'height': 'inherit'})
@@ -186,7 +216,9 @@ tab4 = html.Div([
                                id='d2-mera-filter',
                                options=[{'label': i, 'value': i} for i in mere],
                                value='Incidenca',
-                               clearable=False)
+                               clearable=False),
+                           html.H5('Komentar:'),
+                           html.Div([dcc.Markdown(komentar2)], style={'padding': '0px 0px 20px 0px'})
                            ]),
         html.Div(className="nine columns", style={'height': '1000px'},
                  children=[dcc.Graph(id='plot2', style={'height': 'inherit'})
@@ -214,13 +246,6 @@ def render_content(tab):
 def callback_plot1(spol_value):
     fig = plots1[spol_value]
     return fig
-
-@app.callback(
-    Output('komentarji-p1', 'children'),
-    [dash.dependencies.Input('d1-spol-filter', 'value')])
-def update_output(spol_value):
-    return dcc.Markdown(besedilo_dict_p1[spol_value])
-
 
 @app.callback(
     Output(component_id='plot2', component_property='figure'),
